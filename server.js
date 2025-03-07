3325a9b8ca593bbebe95cc00fef4e1497f0a0de3
Kathiravanscode <Kathiravanscode@gmail.com>
2025-03-07 22:54:48 +0530

    Complted todays task

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ import cors from "cors";
 import path from "path";
 import authRoutes from "./routes/authRoutes.js";
 import petRoutes from "./routes/petRoutes.js";
-
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
 
+// Connect to MongoDB
+connectDB();
+
 // Middleware
-app.use(express.json()); // Parse JSON bodies
-app.use(cors()); // Enable CORS
+app.use(express.json()); 
+app.use(cors()); 
 
 // Serve uploaded images as static files
 app.use("/uploads", express.static(path.resolve("uploads")));
@@ -21,12 +24,8 @@ app.use("/uploads", express.static(path.resolve("uploads")));
 app.use("/api/auth", authRoutes); // User authentication routes
 app.use("/api/pets", petRoutes); // Pet reporting routes
 
-// Database Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB Connection Error:", err));
 
-// Start Server
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
